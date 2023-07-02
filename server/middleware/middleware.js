c8ae96126f5ad887a0ea8854bb3bdf46afb74078
Rafael Ferreira <rafaelferreira@MacBook-Pro.Home>
2023-07-02 22:48:44 +0100

    Creating middleware for auth and routes

---
+++ server/middleware/middleware.js
@@ -0,0 +1,48 @@
+const jwt = require('jsonwebtoken');
+const Agent = require('../models/Agent');
+const SECRET_KEY = '5eb63d320b1a2c1306a6f8d3fe3abc5d';
+const FULL_ROLE_ACCESS = 'agent';
+
+const isRole = (role) => async (req, res, next) => {
+  if(!req.headers.authorization){
+    return res.status(401).json({ message: 'Missing authorization token' });
+  }
+
+  const token = req.headers?.authorization.split(' ')[1];
+  console.log(token);
+
+  if (!token) {
+    return res.status(401).json({ message: 'Missing authorization token' });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, SECRET_KEY);
+    const agentId = decodedToken._id;
+    const agent = await Agent.findOne({ _id: agentId });
+
+    if (!agent) {
+      return res.status(401).json({ message: `Invalid ${role}` });
+    }
+
+    if (agent.role !== role) {
+      return res.status(403).json({ message: 'You are not authorized to perform this operation' });
+    }
+
+    if(agent.role === FULL_ROLE_ACCESS){
+      req.agentId = agentId;  
+      next();
+      return;
+    }
+
+    req.agentId = agentId;
+    next();
+  } catch (error) {
+    console.log(error.message); // Log the error message
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+const isAgent = isRole('agent');
+const isPassenger = isRole('passenger');
+
+module.exports = { isAgent, isPassenger };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,23 @@
-/**
-* App routes definitions.
-*/
 'use strict';
 
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
+const ProductController = require('../controller/ProductController.js')
+const AgentController = require('../controller/AgentController.js')
+const {isAgent , isPassenger} = require('../middleware/middleware');
 
 // To confirm setup only.
-router.get('/', function(req, res) { return res.send('Hello world!'); });
+router.get('/', function(req, res) { 
+  return res.send('Hello world!'); 
+});
 
+// Product routes
+router.post('/product', isAgent, (req, res) => ProductController.createProduct(req, res));
+router.get('/product', isPassenger, (req, res) => ProductController.listProducts(req, res));
+router.delete('/product/:id', isAgent, (req, res) => ProductController.deleteProduct(req, res));
+router.post('/product/search', isPassenger, (req, res) => ProductController.searchProduct(req, res));
+
+// Agent routes
+router.post('/agent/login', (req, res, next) => AgentController.login(req, res, next));
+router.post('/agent/signup', (req, res, next) => AgentController.signup(req, res, next));
 module.exports = router;
